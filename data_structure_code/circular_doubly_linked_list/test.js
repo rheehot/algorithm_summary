da1e4bce21d83ee32560e381194a07e5ded4aeaf
janghyoseog <bestdevhyo1225@gmail.com>
2019-06-26 17:01:56 +0900

    test: Simulate a delete First item

--- data_structure_code/circular_doubly_linked_list/test.js
+++ data_structure_code/circular_doubly_linked_list/test.js
@@ -1,32 +1,32 @@
 const LinkedList = require('./linked_list');
 const linkedList = new LinkedList();
 
-describe('LinkedList Test', () => {
-    test('add First Test', () => {
+describe('Linked List test', () => {
+    test('Add first test', () => {
         linkedList.addFirst(1);
         expect(linkedList.getItem(0)).toEqual(1);
         linkedList.addFirst(2);
         expect(linkedList.getItem(0)).toEqual(2);
     });
 
-    test('add Last Test', () => {
+    test('Add last test', () => {
         linkedList.addLast(3);
         expect(linkedList.getItem(linkedList.size()-1)).toEqual(3);
     });
 
-    test('add First Test', () => {
+    test('Add first test', () => {
         linkedList.addFirst(4);
         expect(linkedList.getItem(0)).toEqual(4);
     });
 
-    test('add Last Test', () => {
+    test('Add last test', () => {
         linkedList.addLast(5);
         expect(linkedList.getItem(linkedList.size()-1)).toEqual(5);
         linkedList.addLast(6);
         expect(linkedList.getItem(linkedList.size()-1)).toEqual(6);
     });
 
-    test('add(index, item) Test', () => {
+    test('add(index, item) test', () => {
         linkedList.add(2, 7);
         expect(linkedList.getItem(2)).toEqual(7);
         linkedList.add(5, 8);
@@ -39,6 +39,15 @@ describe('LinkedList Test', () => {
         expect(linkedList.getItem(1)).toEqual(20);
         linkedList.add(7, 25);
         expect(linkedList.getItem(7)).toEqual(25);
+    });
+
+    test(`Delete first item test`, () => {
+        linkedList.deleteFirst();
+        expect(linkedList.getItem(0)).toEqual(20);
+        linkedList.deleteFirst();
+        expect(linkedList.getItem(0)).toEqual(10);
+        linkedList.deleteFirst();
+        expect(linkedList.getItem(0)).toEqual(4);
     });
 
     test('Print Item', () => {
